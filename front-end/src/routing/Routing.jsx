1f0df6bfe75f65810b3a7dc92fcad5bd437ba9bf
Đặng Xuân Chiến <xuanchienk58@gmail.com>
2021-10-03 18:39:02 +0700

    Chien commit

--- front-end/src/routing/Routing.jsx
+++ front-end/src/routing/Routing.jsx
@@ -6,15 +6,18 @@ import CourseCreate from '../components/layout/course/CourseCreate'
 import Home from '../components/layout/home/Home'
 import ProtectedRoute from './ProtectedRoute'
 import AuthRoute from './AuthRoute'
-
+import CourseEnrolled from '../components/module/learner/CourseEnrolled'
 
 const Routes = () => {
     return (
         <>
-            <Route path='/teacher/course/create' component={CourseCreate} exact/>
+            {/* <Route path='/teacher/course/create' component={CourseCreate} exact/>
             <ProtectedRoute path='/' component={Home} exact/>
             <AuthRoute path='/signin' component={SignIn} exact/>
-            <AuthRoute path='/signup' component={SignUp} exact/>
+            <AuthRoute path='/signup' component={SignUp} exact/> */}
+            <Route path='/' component={CourseEnrolled} exact/>
+            <Route path='/signin' component={SignIn} exact/>
+            <Route path='/signup' component={SignUp} exact/>
         </>
     )
 }
