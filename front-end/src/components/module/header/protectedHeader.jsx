a612611a5827d7928854fd3b481554c32dcc1412
Tony <75075009+Duonglam44@users.noreply.github.com>
2021-10-03 09:55:13 +0700

    Revert "commit"

--- front-end/src/components/module/header/protectedHeader.jsx
+++ front-end/src/components/module/header/protectedHeader.jsx
@@ -1,10 +1,11 @@
 import "./header.scss"
-import React from "react"
+import React, { useState, useContext } from "react"
 import { Container, Nav } from "react-bootstrap"
+import Offcanvas from "react-bootstrap/Offcanvas"
 import { Link } from "react-router-dom"
 import { BiSearch } from "react-icons/bi"
-import { BsBell } from "react-icons/bs"
-import { FiMail } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi"
+import { BsBell, BsCaretDownFill } from "react-icons/bs"
 import logo from "../../../assets/images/logoGuru.png"
 import avatar from "../../../assets/images/avatar.png"
 import { UserContext } from "../../../context/userContext"
@@ -18,52 +19,23 @@ const protectedHeader = () => {
             <img src={logo} alt="logo Guru Academy" />
           </Link>
         </div>
-        <Nav className="header__menu d-none d-lg-flex" style={{flexGrow: '0', margin: '0'}}>
-          <Nav.Item>
-            <Link to="/course" className="nav-link">
-              for you
-            </Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Link to="/course" className="nav-link">
-              live stream
-            </Link>
-          </Nav.Item>
-          <form action="" className="header__menu-form" >
-            <input type="text"/>
-            <div className='button header__menu-form-button'>
+        <Nav className="header__menu d-none d-lg-flex">
+          <Link to='/course'>
+            for you
+          </Link>
+          <Link to='/course'>
+            live stream
+          </Link>
+          <form action="">
+            <input type="text" />
+            <div>
               <BiSearch />
             </div>
           </form>
-          <Nav.Item
-            style={{
-              border: '1px solid #ccc',
-              borderRadius: '25px',
-            }}
-          >
-            <Link to="/" className="nav-link">
-              share your work
-            </Link>
-          </Nav.Item>
+          <Link to='/'>
+            share your work
+          </Link>
         </Nav>
-
-        <div className="header__search d-none d-lg-flex">
-        <button className="button header__search-button">
-            <FiMail />
-          </button>
-
-          <button className="button header__search-button">
-            <BsBell />
-          </button>
-          <div
-            className="d-flex align-items-center px-2"
-            style={{ height: "50px" }}
-          >
-            <div className="header__avatar me-3" style={{ cursor: "pointer" }}>
-              <img src={avatar} alt="" />
-            </div>
-          </div>
-        </div>
       </Container>
     </div>
   )
